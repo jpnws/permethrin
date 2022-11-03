b11135da71ffb0efac7ca285f0d975936cac2792
Ji Park <ji@jipark.dev>
2022-11-03 14:10:57 +1100

    Update root page to be the landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main></main>;
+import Link from 'next/link';
+
+export default async function Landing() {
+  return (
+    <main>
+      <Link href="/dashboard/">Dashboard</Link>
+    </main>
+  );
 }

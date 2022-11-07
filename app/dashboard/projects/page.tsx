c436fe723034f7b50ec48aafa45fce73e7edd977
Ji Park <ji@jipark.dev>
2022-11-07 12:49:59 +1100

    Basic Temor table implementation.

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,13 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 
+import { Table, TableHead, TableHeaderCell, TableBody, TableRow, TableCell } from '@tremor/react';
+
 export default async function Projects() {
   const prisma = new PrismaClient();
   const projects = await prisma.project.findMany();
   return (
     <main>
-      {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
-      ))}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>Name</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {projects.map((project) => (
+            <TableRow key={project.id}>
+              <TableCell>{project.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </main>
   );
 }

6a6e6b61cd6116cdcc7a6c17dc70bdc902eafd80
Ji Park <ji@jipark.dev>
2022-11-08 18:40:58 +1100

    Continue pagination implementation.

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -2,18 +2,29 @@ import { PrismaClient } from '@prisma/client';
 import ProjectsTable from 'app/dashboard/projects/projects-table';
 import superjson from 'superjson';
 
-export default async function Projects() {
+export default async function Projects({ searchParams }: { searchParams: { page: string } }) {
   const prisma = new PrismaClient();
-  const projects = await prisma.project.findMany({
-    take: 10,
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
+  const projects = searchParams.page
+    ? await prisma.project.findMany({
+        skip: parseInt(searchParams.page) * 20,
+        take: 20,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      })
+    : await prisma.project.findMany({
+        take: 20,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+  const page = searchParams.page ? parseInt(searchParams.page) + 1 : 2;
   const projectJson = superjson.stringify(projects);
   return (
     <main>
-      <ProjectsTable projects={projectJson} />
+      <h1>Projects</h1>
+      {/* @ts-expect-error Server Component */}
+      <ProjectsTable projects={projectJson} page={page} />
     </main>
   );
 }
--- app/dashboard/projects/projects-table.tsx
+++ app/dashboard/projects/projects-table.tsx
@@ -1,22 +1,19 @@
-'use client';
-
 import { Project } from '@prisma/client';
+import Link from 'next/link';
 import superjson from 'superjson';
 
-export default function ProjectsTable({ projects }: { projects: string }) {
+export default async function ProjectsTable({ projects, page }: { projects: string; page: string }) {
   const data = superjson.parse<Project[]>(projects);
   return (
     <div>
-      <h1>Projects</h1>
-      <div>
-        {data.map((project) => (
-          <div key={project.id} className="grid grid-cols-3">
-            <div className="">{project.name}</div>
-            <div className="truncate">{project.description}</div>
-            <div className="">{project.status}</div>
-          </div>
-        ))}
-      </div>
+      {data.map((project) => (
+        <div key={project.id} className="grid grid-cols-4">
+          <div className="">{project.name}</div>
+          <div className="truncate">{project.description}</div>
+          <div className="">{project.status}</div>
+        </div>
+      ))}
+      <Link href={`/dashboard/projects?page=${page}`}>Next</Link>
     </div>
   );
 }

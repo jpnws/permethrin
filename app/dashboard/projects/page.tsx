161cf96c72b9affb465264df64adc14f83046e5a
Ji Park <ji@jipark.dev>
2022-11-08 20:50:42 +1100

    Further pagination code.

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,30 +1,30 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient, Project } from '@prisma/client';
 import ProjectsTable from 'app/dashboard/projects/projects-table';
 import superjson from 'superjson';
+import { createPaginator } from 'app/dashboard/paginate';
+
+const paginate = createPaginator({ perPage: 20 });
 
 export default async function Projects({ searchParams }: { searchParams: { page: string } }) {
   const prisma = new PrismaClient();
-  const projects = searchParams.page
-    ? await prisma.project.findMany({
-        skip: parseInt(searchParams.page) * 20,
-        take: 20,
+  const result = superjson.stringify(
+    await paginate<Project, Prisma.ProjectFindManyArgs>(
+      prisma.project,
+      {
         orderBy: {
           createdAt: 'desc',
         },
-      })
-    : await prisma.project.findMany({
-        take: 20,
-        orderBy: {
-          createdAt: 'desc',
-        },
-      });
-  const page = searchParams.page ? parseInt(searchParams.page) + 1 : 2;
-  const projectJson = superjson.stringify(projects);
+      },
+      {
+        page: searchParams.page,
+      },
+    ),
+  );
   return (
     <main>
       <h1>Projects</h1>
       {/* @ts-expect-error Server Component */}
-      <ProjectsTable projects={projectJson} page={page} />
+      <ProjectsTable props={result} />
     </main>
   );
 }
--- app/dashboard/projects/projects-table.tsx
+++ app/dashboard/projects/projects-table.tsx
@@ -1,19 +1,21 @@
-import { Project } from '@prisma/client';
+import { Prisma, Project } from '@prisma/client';
+import { PaginatedResult } from 'app/dashboard/paginate';
 import Link from 'next/link';
 import superjson from 'superjson';
 
-export default async function ProjectsTable({ projects, page }: { projects: string; page: string }) {
-  const data = superjson.parse<Project[]>(projects);
+export default async function ProjectsTable({ props }: { props: string }) {
+  const res = superjson.parse<PaginatedResult<Project>>(props);
+
   return (
     <div>
-      {data.map((project) => (
-        <div key={project.id} className="grid grid-cols-4">
-          <div className="">{project.name}</div>
-          <div className="truncate">{project.description}</div>
-          <div className="">{project.status}</div>
+      {res.data.map((item) => (
+        <div key={item.id} className="grid grid-cols-4">
+          <div className="">{item.name}</div>
+          <div className="truncate">{item.description}</div>
+          <div className="">{item.status}</div>
         </div>
       ))}
-      <Link href={`/dashboard/projects?page=${page}`}>Next</Link>
+      <Link href={`/dashboard/projects?page=${res.meta.next}`}>Next</Link>
     </div>
   );
 }

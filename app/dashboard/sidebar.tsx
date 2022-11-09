4c591a30dff47e41f5be505f4a0274fa9828d9ab
Ji Park <ji@jipark.dev>
2022-11-09 20:51:10 +1100

    Update some sidebar styles.

--- app/dashboard/sidebar.tsx
+++ app/dashboard/sidebar.tsx
@@ -3,16 +3,19 @@ import Image from 'next/image';
 
 export default function Sidebar() {
   return (
-    <aside className="rounded border">
-      <Image src="/permethrin.png" alt="Permethrin" width={50} height={50} priority />
-      <ul>
-        <li>
-          <Link href="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/dashboard/projects">Projects</Link>
-        </li>
-      </ul>
+    <aside className="flex flex-col gap-y-2 rounded p-2">
+      <div className="flex gap-x-2">
+        <Image src="/permethrin.png" alt="Permethrin" width={50} height={50} priority />
+        <div className="pt-2 font-bold">Permethrin</div>
+      </div>
+      <div className="flex flex-col gap-y-2">
+        <Link href="/dashboard" className="rounded px-2 hover:bg-gray-100">
+          Dashboard
+        </Link>
+        <Link href="/dashboard/projects" className="rounded px-2 hover:bg-gray-100">
+          Projects
+        </Link>
+      </div>
     </aside>
   );
 }

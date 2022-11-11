9458c999c9eabd36bb53d6cc78036b9d0e1adff3
Ji Park <ji@jipark.dev>
2022-11-11 13:55:58 +1100

    Apply mona sans font.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,12 @@
 import Sidebar from 'app/dashboard/sidebar';
 
+import localFont from '@next/font/local';
+
+const monaSans = localFont({ src: '../../public/fonts/Mona-Sans.woff2' });
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-[1fr,minmax(auto,12rem),minmax(25rem,81rem),1fr] gap-x-8 py-8">
+    <div className={`grid grid-cols-[1fr,minmax(auto,12rem),minmax(25rem,81rem),1fr] gap-x-8 py-8 ${monaSans.className}`}>
       <div className="col-start-2">
         <Sidebar />
       </div>

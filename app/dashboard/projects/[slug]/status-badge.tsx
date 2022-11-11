8b40409fc9374efc5033f38848d76d17f609332b
Ji Park <ji@jipark.dev>
2022-11-11 13:32:30 +1100

    Add project status badge component.

---
+++ app/dashboard/projects/[slug]/status-badge.tsx
@@ -0,0 +1,21 @@
+function badgeStyle(status: string) {
+  switch (status) {
+    case 'ACTIVE':
+      return 'bg-green-100 text-green-800';
+    case 'INACTIVE':
+      return 'bg-gray-100 text-gray-800';
+    case 'COMPLETED':
+      return 'bg-blue-100 text-blue-800';
+    default:
+      return '';
+  }
+}
+
+export default function StatusBadge({ status }: { status: string }) {
+  const bgStyle = badgeStyle(status);
+  return (
+    <div className="flex items-center">
+      <div className={`rounded-md px-1.5 text-sm ${bgStyle}`}>{status.toLowerCase()}</div>
+    </div>
+  );
+}

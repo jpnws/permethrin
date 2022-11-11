05d465ea7c822c4fa3db4c76c0917a9a1ff08a9b
Ji Park <ji@jipark.dev>
2022-11-12 08:52:25 +1100

    Remove eslint functional.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 const config = {
   plugins: [
-    'functional', // npm install --save-dev eslint @typescript-eslint/parser tsutils eslint-plugin-functional
     'tailwindcss', // npm install --save-dev eslint eslint-plugin-tailwindcss
   ],
   extends: [
     'next/core-web-vitals',
-    'plugin:functional/recommended', // npm install --save-dev eslint @typescript-eslint/parser tsutils eslint-plugin-functional
     'plugin:tailwindcss/recommended', // npm install --save-dev eslint eslint-plugin-tailwindcss
     'prettier', // npm install --save-dev eslint-config-prettier (must be last in the extends array)
   ],

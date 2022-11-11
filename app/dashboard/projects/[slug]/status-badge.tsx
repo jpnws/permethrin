aa27ab373a0fb5e47d33cb8ba0343bda02abf458
Ji Park <ji@jipark.dev>
2022-11-11 13:54:45 +1100

    Fix completed to archived case.

--- app/dashboard/projects/[slug]/status-badge.tsx
+++ app/dashboard/projects/[slug]/status-badge.tsx
@@ -4,7 +4,7 @@ function badgeStyle(status: string) {
       return 'bg-green-100 text-green-800';
     case 'INACTIVE':
       return 'bg-gray-100 text-gray-800';
-    case 'COMPLETED':
+    case 'ARCHIVED':
       return 'bg-blue-100 text-blue-800';
     default:
       return '';

0ca0633762fb8484b26b766041c985432265f461
Ji Park <ji@jipark.dev>
2022-11-11 13:32:49 +1100

    Update the dashboard layout. Make it wider.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from 'app/dashboard/sidebar';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-[1fr,minmax(auto,15rem),min(57rem,100%),1fr] gap-x-8 py-8">
+    <div className="grid grid-cols-[1fr,minmax(auto,12rem),minmax(25rem,81rem),1fr] gap-x-8 py-8">
       <div className="col-start-2">
         <Sidebar />
       </div>

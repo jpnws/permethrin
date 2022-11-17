2660f0d671c2abde71d750b13068def6e3705fa8
Ji Park <ji@jipark.dev>
2022-11-18 00:05:38 +0700

    Display project status badges.

--- app/projects/projects-table.tsx
+++ app/projects/projects-table.tsx
@@ -1,13 +1,14 @@
 import { Project } from '@prisma/client';
+import { formatDistance } from 'date-fns';
 import Link from 'next/link';
-import { formatRelative, formatDistance, subDays } from 'date-fns';
+import StatusBadge from 'app/projects/status-badge';
 
 export default async function ProjectsTable({ projects }: { projects: Project[] }) {
   return (
     <div className="text-sm">
       {projects.map((project) => (
         <Link key={project.id} href={`/projects/${encodeURIComponent(project.slug as string)}`} className="grid grid-cols-[10rem_auto_auto] border-b p-2 hover:bg-gray-100">
-          <div className="">{project.status}</div>
+          <StatusBadge status={project.status} />
           <div className="flex justify-start">{project.name}</div>
           <div className="flex justify-end">{formatDistance(project.createdAt, Date.now(), { addSuffix: true })}</div>
         </Link>

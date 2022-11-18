524bfa2c35d5618aaa78544528a93ab10c80bf35
Ji Park <ji@jipark.dev>
2022-11-18 22:02:33 +0700

    Add border and reorganize divs for the info column.

--- app/projects/[slug]/project-information.tsx
+++ app/projects/[slug]/project-information.tsx
@@ -4,13 +4,11 @@ import { Project, ProjectAttachment, Ticket } from '@prisma/client';
 
 export default async function ProjectInformation({ project, attachments, tickets }: { project: Project; attachments: ProjectAttachment[]; tickets: Ticket[] }) {
   return (
-    <div>
-      <div className="border-b">
-        <div className="p-4">
-          <div className="text-lg font-medium">{project.name}</div>
-          <div className="text-sm text-gray-500">{project.description}</div>
-          <div className="text-sm">Number of attachments: {attachments.length}</div>
-        </div>
+    <div className="h-full border-r">
+      <div className="p-4">
+        <div className="text-lg font-medium">{project.name}</div>
+        <div className="text-sm text-gray-500">{project.description}</div>
+        <div className="text-sm">Number of attachments: {attachments.length}</div>
       </div>
     </div>
   );

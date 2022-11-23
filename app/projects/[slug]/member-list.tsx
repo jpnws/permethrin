ba78d3bdc5f94407b288670a16c5001cad533fbd
Ji Park <ji@jipark.dev>
2022-11-24 10:06:23 +1100

    Add member name as well.

--- app/projects/[slug]/member-list.tsx
+++ app/projects/[slug]/member-list.tsx
@@ -6,12 +6,15 @@ import Image from 'next/image';
 export default async function MemberList({ members }: { members: User[] }) {
   return (
     <div className="col-span-2 flex flex-col gap-y-4">
-      <div className="text-sm font-bold text-gray-500">Project Members</div>
+      <div className="text-sm font-bold text-gray-500">Project members</div>
       <div className="flex flex-col gap-y-2">
         {members.map((member) => (
           <div key={member.id} className="flex items-center gap-x-2">
-            <Image src={member.avatar} alt={member.username} width={32} height={32} className="rounded-full" />
-            <div className="text-sm">{member.username}</div>
+            <Image src={member.avatar} alt={member.username} width={34} height={34} className="rounded-full" />
+            <div className="flex flex-col">
+              <div className="text-sm font-bold text-gray-800">{member.name}</div>
+              <div className="text-sm text-gray-600">@{member.username}</div>
+            </div>
           </div>
         ))}
       </div>

c7d4b60d9bb5171831faafe9cdd57d7ce2fb7411
Ji Park <ji@jipark.dev>
2022-11-24 19:06:36 +1100

    Reimplement padding and borders.

--- app/projects/[slug]/project-details.tsx
+++ app/projects/[slug]/project-details.tsx
@@ -15,15 +15,17 @@ export default async function ProjectDetails({ project, tickets, members }: { pr
   const creator = await getCreator(project.creatorId);
   const ticketCount = tickets.length;
   return (
-    <div className="grid h-fit grid-cols-2 grid-rows-3 gap-y-4 p-4">
-      <div className="flex items-center text-sm font-bold text-gray-500">Status</div>
-      <StatusBadge status={project.status} />
-      <div className="flex items-center text-sm font-bold text-gray-500">Creator</div>
-      <div className="flex items-center text-sm">{creator?.name}</div>
-      <div className="flex items-center text-sm font-bold text-gray-500">Number of tickets</div>
-      <div className="flex items-center text-sm">{ticketCount}</div>
-      {/* @ts-expect-error Server Component */}
-      <MemberList members={members} />
+    <div className="border-l">
+      <div className="grid h-fit grid-cols-2 grid-rows-3 gap-y-4 p-4">
+        <div className="flex items-center text-sm font-bold text-gray-500">Status</div>
+        <StatusBadge status={project.status} />
+        <div className="flex items-center text-sm font-bold text-gray-500">Creator</div>
+        <div className="flex items-center text-sm">{creator?.name}</div>
+        <div className="flex items-center text-sm font-bold text-gray-500">Number of tickets</div>
+        <div className="flex items-center text-sm">{ticketCount}</div>
+        {/* @ts-expect-error Server Component */}
+        <MemberList members={members} />
+      </div>
     </div>
   );
 }
--- app/projects/[slug]/project-information.tsx
+++ app/projects/[slug]/project-information.tsx
@@ -4,12 +4,10 @@ import { Project, ProjectAttachment, Ticket } from '@prisma/client';
 
 export default async function ProjectInformation({ project, attachments }: { project: Project; attachments: ProjectAttachment[] }) {
   return (
-    <div className="h-full border-r">
-      <div className="p-4">
-        <div className="text-lg font-medium">{project.name}</div>
-        <div className="text-sm text-gray-500">{project.description}</div>
-        <div className="text-sm">Number of attachments: {attachments.length}</div>
-      </div>
+    <div className="h-full px-8 py-4">
+      <div className="text-lg font-medium">{project.name}</div>
+      <div className="text-sm text-gray-500">{project.description}</div>
+      <div className="text-sm">Number of attachments: {attachments.length}</div>
     </div>
   );
 }

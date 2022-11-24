4730635f153641ed9a657e75fb6971fa604ddc62
Ji Park <ji@jipark.dev>
2022-11-24 19:17:16 +1100

    Remove attachment number and add gap between sections.

--- app/projects/[slug]/project-information.tsx
+++ app/projects/[slug]/project-information.tsx
@@ -4,10 +4,9 @@ import { Project, ProjectAttachment, Ticket } from '@prisma/client';
 
 export default async function ProjectInformation({ project, attachments }: { project: Project; attachments: ProjectAttachment[] }) {
   return (
-    <div className="h-full px-8 py-4">
+    <div className="flex h-full flex-col gap-y-4 px-8 py-4">
       <div className="text-lg font-medium">{project.name}</div>
-      <div className="text-sm text-gray-500">{project.description}</div>
-      <div className="text-sm">Number of attachments: {attachments.length}</div>
+      <div className="text-sm text-gray-800">{project.description}</div>
     </div>
   );
 }
